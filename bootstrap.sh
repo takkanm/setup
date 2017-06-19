8785d9f81495d2b272aa869a301cff6fd75486b4
takkanm <takkanm@gmail.com>
2017-06-19 11:15:55 +0900

    add keyrepeat setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,6 @@ pushd ~/homebrew
 curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 popd
 brew tap caskroom/cask
+
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
